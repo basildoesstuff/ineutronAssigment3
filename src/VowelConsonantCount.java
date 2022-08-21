25cfae95a63d6e8c0d806a0c129fe8a1e5f060b8
Basil <contact.basilsunny@gmail.com>
2022-08-21 23:22:16 +0530

    Question 7

---
+++ src/VowelConsonantCount.java
@@ -0,0 +1,17 @@
+public class VowelConsonantCount {
+    public static void main(String[] args) {
+        int vCount = 0, cCount = 0;
+        String str = "Hello guys I am basil";
+        str = str.toLowerCase();
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                vCount++;
+            }
+            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {
+                cCount++;
+            }
+        }
+        System.out.println("Number of vowels: " + vCount);
+        System.out.println("Number of consonants: " + cCount);
+    }
+}
